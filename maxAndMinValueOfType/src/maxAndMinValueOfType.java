428a36dc06c31f5449af3370d9c41ea42fcc0290
IMalyanova <53153912+IMalyanova@users.noreply.github.com>
2019-10-15 18:59:53 +0300

    1

---
+++ maxAndMinValueOfType/src/maxAndMinValueOfType.java
@@ -0,0 +1,22 @@
+public class maxAndMinValueOfType {
+    public static void main(String[] args) {
+        double v = 24.0 * 0.1;
+        System.out.println(v);
+
+        System.out.println("Double");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        System.out.println("Short");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+
+        System.out.println("Long");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        System.out.println("Float");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+    }
+}
